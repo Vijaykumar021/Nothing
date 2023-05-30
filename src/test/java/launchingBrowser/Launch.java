11af4724bb3b6ba64cc3d2546e238f64f139759b
Vijaykumar021 <vijayselvaraj979@gmail.com>
2023-05-30 22:26:41 +0530

    nothing..

--- src/test/java/launchingBrowser/Launch.java
+++ src/test/java/launchingBrowser/Launch.java
@@ -4,27 +4,26 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Launch {
-public static WebDriver driver;
+	public static WebDriver driver;
+
 	public static void main(String[] args) {
-		
+
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
+
 		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
-		
-		
-		driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[1]/div[1]/div[2]/div[2]/form/div/div/input")).sendKeys("iphone");
+
+		driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[1]/div[1]/div[2]/div[2]/form/div/div/input"))
+				.sendKeys("iphone");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.close();
-		
-		
 
 	}
 
